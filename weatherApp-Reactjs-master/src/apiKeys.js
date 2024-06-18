c2768a81bcbbe7890414365aa762b3f917cdb443
Mohammad Afreen Aqthar <110152840+md-afreen-aqthar@users.noreply.github.com>
2024-06-19 00:54:23 +0530

    first commit

---
+++ weatherApp-Reactjs-master/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f89c42e665aa3f8559c3aa49bf6f0b15",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
